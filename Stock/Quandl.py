0eb37af9eac8292c575c0d5d82286254c97002cf
Brian Cole <Brian Cole>
2016-08-09 00:47:32 -0400

    -

--- Stock/Quandl.py
+++
@@ -1,4 +0,0 @@
-
-import urllib2
-mydata = urllib2.urlopen('https://www.quandl.com/api/v3/datasets/NASDAQ/AAPL_NET_INC.csv?api_key=WiJ7yFp1hjJaX77v1DZs').read()
-print mydata
--- Stock/Trade_Help/Quandl.py
+++
@@ -1,3 +0,0 @@
-import quandl
-mydata = quandl.get_table('ZACKS/FC', ticker='AAPL')
-print mydata
